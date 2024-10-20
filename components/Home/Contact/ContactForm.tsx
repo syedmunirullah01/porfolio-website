c2ed28c5c4e660ed45242d0d408852749d1ffe78
Syed Munir Ullah <syedmunirullah01@gmail.com>
2024-10-21 03:53:18 +0500

    update

--- components/Home/Contact/ContactForm.tsx
+++ components/Home/Contact/ContactForm.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const ContactForm = () => {
   return (
     <div className="bg-[#140c1c] rounded-lg p-4 sm:p-10">
       <h1 className="text-bg text-2xl md:text-3xl lg:text-[2.5rem] font-bold">
-        Let's Work Together
+        Let&apos;s Work Together
       </h1>
       <p className="text-gray-200 mt-3 lg:text-base text-xs md:text-sm">
-        Ready to collaborate or have questions? Fill out the form, and let's
-        turn your ideas into reality. I'm excited to connect and explore
-        possibilities together!
+        Ready to collaborate or have questions? Fill out the form, and
+        let&apos;s turn your ideas into reality. I&apos;m excited to connect and
+        explore possibilities together!
       </p>
 
       <form className="mt-8 block w-full overflow-hidden">
@@ -17,28 +17,24 @@ const ContactForm = () => {
           <input
             type="text"
             placeholder="First Name"
-            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full
-                  "
+            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full"
           />
           <input
             type="text"
             placeholder="Last Name"
-            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full
-                  "
+            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full"
           />
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between gap-4 mt-5">
           <input
             type="text"
             placeholder="Email Address"
-            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full
-                  "
+            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full"
           />
           <input
             type="text"
             placeholder="Phone Number"
-            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full
-                  "
+            className="flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full"
           />
         </div>
         <div>
@@ -53,7 +49,8 @@ const ContactForm = () => {
         </div>
         <textarea
           className="w-full mt-5 bg-black text-white placeholder:text-gray-600 px-4 py-3.5 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none"
-         rows={7} placeholder='Message'
+          rows={7}
+          placeholder="Message"
         ></textarea>
         <div className="mt-4">
           <button className="px-8 py-3.5 bg-[#7947df] text-white hover:bg-[#5c2fb7] transition-all duration-150 rounded-full">
@@ -63,6 +60,6 @@ const ContactForm = () => {
       </form>
     </div>
   );
-}
+};
 
-export default ContactForm
+export default ContactForm;
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,65 +1,74 @@
 "use client";
 
-import { navLinks } from '@/constant/constant'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import { HiBars3BottomRight } from 'react-icons/hi2';
+import { navLinks } from "@/constant/constant";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { HiBars3BottomRight } from "react-icons/hi2";
+import Image from "next/image"; // Import the Image component
 
 type Props = {
-    openNav: () => void;
-    
-}
+  openNav: () => void;
+};
+
 const Nav = ({ openNav }: Props) => {
-    const [navBg, setnavBg] = useState(false);
-    useEffect(() => {
-        const handler = () => {
-            if (window.scrollY >= 90) {
-                setnavBg(true)
-            }
-            if (window.scrollY < 90) {
-                setnavBg(false)
-            }
-        };
+  const [navBg, setnavBg] = useState(false);
 
-        window.addEventListener("scroll", handler)
-        
-        return () => {
-            window.removeEventListener("scroll", handler);
-        }
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) {
+        setnavBg(true);
+      } else {
+        setnavBg(false);
+      }
+    };
 
-    },[])
+    window.addEventListener("scroll", handler);
+
+    return () => {
+      window.removeEventListener("scroll", handler);
+    };
+  }, []);
+
   return (
-    <div className={`fixed ${navBg ? "bg-[#240b39]" : "fixed"} h-[12vh] z-[10]  w-full`}>
+    <div
+      className={`fixed ${
+        navBg ? "bg-[#240b39]" : "fixed"
+      } h-[12vh] z-[10] w-full`}
+    >
       <div className="flex items-center h-full justify-between w-[95%] sm:w-[90%] xl:w-[80%] mx-auto">
         {/* Logo */}
-        <img
-          src="/images/logo.png"
+        <Image
+          src="/images/logo.png" // Use the Image component
           alt="Logo"
           width={170}
           height={170}
           className="ml-[-1.5rem] sm:ml-0"
+          priority={true} // Add priority for better LCP
         />
-        {/* Nav Links  */}
+        {/* Nav Links */}
         <div className="flex items-center space-x-10">
           <div className="hidden lg:flex items-center space-x-8">
-            {navLinks.map((navlink) => {
-              return (
-                <Link key={navlink.id} href={navlink.url}>
-                  <p className="nav__link">{navlink.label}</p>
-                </Link>
-              );
-            })}
+            {navLinks.map((navlink) => (
+              <Link key={navlink.id} href={navlink.url}>
+                <p className="nav__link">{navlink.label}</p>
+              </Link>
+            ))}
           </div>
           {/* Hire Me Button */}
-          <div className='flex items-center space-x-4'>
-                      <button className='md:px-10 md:py-3 px-8 py-3 text-blue-800 font-semibold sm:text-base text-sm bg-white hover:bg-gray-200 transition-all duration-200 rounded-lg'>Hire Me</button>
-                      {/* burger menu */}
-                      <HiBars3BottomRight onClick={openNav} className='w-8 h-8 text-white cursor-pointer lg:hidden'/>
+          <div className="flex items-center space-x-4">
+            <button className="md:px-10 md:py-3 px-8 py-3 text-blue-800 font-semibold sm:text-base text-sm bg-white hover:bg-gray-200 transition-all duration-200 rounded-lg">
+              Hire Me
+            </button>
+            {/* Burger menu */}
+            <HiBars3BottomRight
+              onClick={openNav}
+              className="w-8 h-8 text-white cursor-pointer lg:hidden"
+            />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Nav
+export default Nav;
