01fb5c62ec4466f1b7f25327800b8070fd7888f2
Syed Munir Ullah <syedmunirullah01@gmail.com>
2024-10-21 03:43:07 +0500

    updated

--- components/Home/Services/Services.tsx
+++ components/Home/Services/Services.tsx
@@ -8,13 +8,13 @@ const Services = () => {
     <div className='pt-16 pb-16 bg-[#0f0715]'>
           <SectionHeading>Services</SectionHeading>
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-[80%] mx-auto items-center mt-20'>
-              {servicesData.map((service) => {
+              {servicesData.map((service,i) => {
                 return (
                   <div
                     key={service.id}
                     data-aos="fade-left"
                     data-aos-anchor-placement="top-center"
-                    data-aos-delay={"${i * 150}"}
+                    data-aos-delay={`${i * 150}`}
                   >
                     <ServiceCard service={service} />
                   </div>
